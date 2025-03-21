22de1080666cd8b455919da9b3fea670862bdca5
lutfensus <tufanyesil@protonmail.com>
2025-03-21 13:19:44 +0100

    First Commit

---
+++ untitled/src/product.java
@@ -0,0 +1,56 @@
+public class product {
+    private int id;
+    private String name;
+    private int price;
+    private String category;
+    private int stockQuantity;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+
+    public String displayInfo() {
+
+        return name+id+price+category+stockQuantity;
+
+
+    }
+}
